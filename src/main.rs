0db09664508bc070c94dbc71f4c9152941c437da
Michael Gattozzi <mgattozzi@gmail.com>
2017-05-16 11:33:20 -0400

    Make code more robust and add licenses
    This adds the necessary licenses that were forgotten in the last commit.
    Also adds error handling to the code such that it's always logged to the
    file but will keep the program running if something goes wrong for
    whatever reason.

--- src/main.rs
+++ src/main.rs
@@ -4,68 +4,84 @@ extern crate slog_term;
 extern crate slog_async;
 extern crate time;
 extern crate egg_mode;
+#[macro_use]
+extern crate error_chain;
 
 use egg_mode::{ Token, KeyPair };
 use egg_mode::tweet::DraftTweet;
 use time::Duration;
-use slog::Drain;
+use slog::{ Drain, Logger };
 
 use std::process::{ Command, Output };
 use std::io::prelude::*;
-use std::io::{ BufReader, Result };
+use std::io::BufReader;
 use std::fs::{ File, OpenOptions };
 
-static MESSAGE: &str =
+error_chain!();
+
+const MESSAGE: &str =
     "A new nightly has been released. See what has changed since the last \
-    one ";
-static GITHUB_URL: &str =
-    "https://github.com/rust-lang/rust/commits/master?since=";
+    one https://github.com/rust-lang/rust/commits/master?since=";
 
 fn main() {
-    let mut curr_version = get_current_version();
-    let one_day = Duration::days(1);
-    let log_path = "tweet.log";
-    let file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .truncate(true)
-        .open(log_path)
-        .unwrap();
+    let log =
+        slog::Logger::root(slog_async::Async::new(
+                slog_term::FullFormat::new(
+                    slog_term::PlainDecorator::new(
+                        OpenOptions::new()
+                        .create(true)
+                        .write(true)
+                        .truncate(true)
+                        .open("tweet.log").unwrap()
+                    )
+                ).build().fuse()
+        ).build().fuse(), o!());
 
-    let decorator = slog_term::PlainDecorator::new(file);
-    let drain = slog_term::FullFormat::new(decorator).build().fuse();
-    let drain = slog_async::Async::new(drain).build().fuse();
-    let _log = slog::Logger::root(drain, o!());
-
-    info!(_log, "Starting up");
-
-    let token = Token::Access {
-        consumer: KeyPair::new(read("consumer.key"), read("consumer.secret")),
-        access: KeyPair::new(read("access.key"), read("access.secret")),
-    };
+    info!(log, "Starting up");
 
     loop {
-        // Check if there is an update and if there is tweet it
-        if update() {
-            let tweet_message = MESSAGE.to_owned() + GITHUB_URL + &curr_version;
-            // Update the current running version
-            curr_version = get_current_version();
-            let tweet = DraftTweet::new(&tweet_message);
-            tweet.send(&token).unwrap();
-            info!(_log, "New update posted");
-        } else {
-            info!(_log, "No update for {}", curr_version);
+        if let Err(ref e) = run(&log) {
+            error!(log, "{}", e);
+            for e in e.iter().skip(1) {
+                error!(log, "Caused by: {}", e);
+            }
+
+            if let Some(backtrace) = e.backtrace() {
+                error!(log, "Backtrace: {:?}", backtrace);
+            }
         }
-        // Wait till tomorrow to do it again
-        std::thread::sleep(one_day.to_std().unwrap());
     }
-
-
 }
 
-fn get_current_version() -> String {
-    let output = rustc(vec!["+nightly", "--version"]).unwrap();
-    let stdout = String::from_utf8(output.stdout).unwrap();
+fn run(log: &Logger) -> Result<()> {
+    // Update the current running version each day
+    let curr_version = get_current_version()?;
+    let token = Token::Access {
+        consumer: KeyPair::new(read("consumer.key")?, read("consumer.secret")?),
+        access: KeyPair::new(read("access.key")?, read("access.secret")?),
+    };
+
+    // Check if there is an update and if there is tweet it
+    if update()? {
+        DraftTweet::new(&[MESSAGE, &curr_version].concat().to_owned())
+            .send(&token)
+            .chain_err(|| "Tweet failed to send")?;
+        info!(log, "New update posted");
+    } else {
+        info!(log, "No update for {}", curr_version);
+    }
+    // Wait till tomorrow to do it again
+    std::thread::sleep(Duration::days(1)
+                       .to_std()
+                       .chain_err(|| "Unable to turn Time into StdTime")?);
+    Ok(())
+}
+
+#[inline(always)]
+fn get_current_version() -> Result<String> {
+    let output = rustc(vec!["+nightly", "--version"])?;
+    let stdout = String::from_utf8(output.stdout)
+                        .chain_err(|| "Unable to convert Vec<u8> to String")?;
     // This extracts the date string from asking what version is in use. Since
     // nightly is based off a YYYY-MM-DD string this is the best way to get it
     let mut done: String =
@@ -73,35 +89,43 @@ fn get_current_version() -> String {
           .filter(|s| s.find('-') != s.rfind('-'))
           .collect();
     done.pop();
-    done
+    Ok(done)
 }
 
-fn update() -> bool {
-    let output = rustup(vec!["update", "nightly"]).unwrap();
-    let stdout = String::from_utf8(output.stdout).unwrap();
+#[inline(always)]
+fn update() -> Result<bool> {
+    let output = rustup(vec!["update", "nightly"])?;
+    let stdout = String::from_utf8(output.stdout)
+                        .chain_err(|| "Unable to convert Vec<u8> to String")?;
 
-    if stdout.contains("unchanged") {
-        false
-    } else {
-        true
-    }
+    Ok(!stdout.contains("unchanged"))
 }
 
-fn rustup(args: Vec<&'static str>) -> Result<Output> {
+#[inline(always)]
+fn rustup(args: Vec<&str>) -> Result<Output> {
     Command::new("rustup")
-        .args(args)
+        .args(&args)
         .output()
+        .chain_err(|| "Unable to execute rustc".to_owned() +
+                      &args.iter().fold(String::new(), |acc, &x| acc + x + " "))
 }
 
+#[inline(always)]
 fn rustc(args: Vec<&str>) -> Result<Output> {
     Command::new("rustc")
-        .args(args)
+        .args(&args)
         .output()
+        .chain_err(|| "Unable to execute rustc".to_owned() +
+                      &args.iter().fold(String::new(), |acc, &x| acc + x + " "))
 }
 
-fn read(path: &str) -> String {
+#[inline(always)]
+fn read(path: &str) -> Result<String> {
     let mut buffer = String::new();
-    let mut reader = BufReader::new(File::open(path).unwrap());
-    let _ = reader.read_line(&mut buffer);
-    buffer.trim().to_owned()
+    let mut reader =
+        BufReader::new(
+            File::open(path)
+                .chain_err(|| "Unable to open ".to_owned() + path)?);
+    reader.read_line(&mut buffer).chain_err(|| "Unable to read from ".to_owned() + path)?;
+    Ok(buffer.trim().to_owned())
 }
